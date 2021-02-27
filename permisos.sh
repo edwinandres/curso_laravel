d3ea8b6ccf3c196be888d8dd58e0788dcf6c9a4c
elexier <pazamoryperdon@gmail.com>
2021-02-27 12:26:14 -0500

    Rutas

---
+++ permisos.sh
@@ -0,0 +1,1 @@
+sudo setfacl -m u:www-data:rwx -R /home/edwin/proyectos/curso_laravel/ && sudo setfacl -m u:edwin:rwx -R /home/edwin/proyectos/curso_laravel/
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,15 @@
 //Route es una clase y get un metodo estatico
 //el primer parametro es la url a donde quiero ir
 //la funcion anonima dice lo que se va a ejecutar al introducir la url
-Route::get('/home', function () {
-    return view('welcome');
-});
+Route::get('/', function () { return "Hemos llegado a la página inicial";});
+Route::get('/nosotros', function () { return "Acerca de nosotros";});
+Route::get('/que hacemos', function () { return "Lo que nosotros hacemos";});
+Route::get('/galeria', function () { return "Galeria de imagenes";});
+Route::get('/enlaces', function () { return "Enlaces externos";});
+
+
+//relacionamos el parametro id de la url con una variable de php
+Route::get('/post/{id}/{nombre}', 
+    function ($id,$nombre) { 
+        return "Este es el post No:".$id." y pertenece al usuario: ".$nombre;
+    })->where('nombre','[a-zA-Z]+');//aqui le estamos diciendo que solo acepta letras mayusculas y minusculas
